094950661b0c54cd0eaf0469a363d2675f600bb8
Gor Ayvazyan <gor.ayvazyan.work@gmail.com>
2024-12-24 13:00:06 +0100

    feat: feat: integrate React Router, Loader component, Tailwind CSS, and shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Suspense, lazy } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Loader from "@/components/Loader";
+
+const Home = lazy(() => import("@/pages/Home"));
+const NotFound = lazy(() => import("@/pages/NotFound"));
+
 function App() {
-    return <>Hello World!</>;
+    return (
+        <Suspense fallback={<Loader />}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </Suspense>
+    );
 }
 
 export default App;
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="flex fixed flex-col items-center justify-center text-center h-full w-full">
+            <h1 className="text-7xl mb-5 font-bold">404</h1>
+            <p className="text-xl">Sorry, the page you are looking for does not exist.</p>
+        </div>
+    );
+};
+
+export default NotFound;
